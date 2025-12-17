e48fcc7a4453ae540f22addf0b0725fa82d4efdc
antso.tech <hei.antso.2007@gmail.com>
2025-12-17 13:53:03 +0300

    ajout des tables dish

--- database/schema.sql
+++ database/schema.sql
@@ -1,4 +1,20 @@
+
+CREATE TYPE dish_category as ENUM ('VEGETABLE', 'ANIMAL', 'MARINE', 'DAILY', 'OTHER');
+CREATE TYPE dish_type as Enum ('START', 'MAIN', 'DESSERT');
+
+CREATE TABLE DISH (
+    id_dish int PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    dish_type dish_type
+
+)
+;
 CREATE TABLE INGREDIENT (
     id int PRIMARY KEY,
-    name VARCHAR(255) not null,
+    name VARCHAR(255) NOT NULL,
+    price NUMERIC(10,2) NOT NULL,
+    category dish_category,
+    id_dish int,
+    CONSTRAINT fk_dish FOREIGN KEY (id_dish) REFERENCES DISH(id_dish) 
+
 )
